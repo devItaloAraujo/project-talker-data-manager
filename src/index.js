1cab006bff3dbdde02c5e9ca6208aa141c6fe30d
Italo Araujo <italojsouza@gmail.com>
2023-10-06 14:29:07 -0300

    feat: GET endpoints talker and talker-id

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
+
+const path = 'src/talker.json';
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  fs.readFile(path, 'utf8', (errFile, file) => {
+  // check for any errors
+    if (errFile) {
+      console.error('Error while reading the file:', errFile);
+      return;
+    }
+    if (file) {
+      const data = JSON.parse(file);
+      // output the parsed data
+      res.status(HTTP_OK_STATUS).json(data);
+    } else {
+      res.status(HTTP_OK_STATUS).json([]);
+    }
+  });
+});
+
+app.get('/talker/:id', (req, res) => {
+  fs.readFile(path, 'utf8', (errFile, file) => {
+    if (file) {
+      const data = JSON.parse(file);
+      const arrayOfIds = data.map((element) => element.id);
+      if (arrayOfIds.includes(Number(req.params.id))) {
+        const result = data[req.params.id - 1];
+        res.status(HTTP_OK_STATUS).json(result);
+      } else {
+        res.status(HTTP_NOT_FOUND_STATUS).json({
+          message: 'Pessoa palestrante não encontrada',
+        });
+      }
+      
+      // output the parsed data
+    } else {
+      res.status(HTTP_OK_STATUS).json([]);
+    }
+  });
 });
 
 app.listen(PORT, () => {
